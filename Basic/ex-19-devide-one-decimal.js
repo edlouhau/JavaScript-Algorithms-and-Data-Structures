a5c636a1f93b1320d84d073ce33f623b895322b7
Eduardo Louhau <louhaudev@gmail.com>
2023-04-03 17:49:47 -0400

    ex-19

---
+++ Basic/ex-19-devide-one-decimal.js
@@ -0,0 +1,4 @@
+// The variable quotient should equal 2.2
+// You should use the / operator to divide 4.4 by 2
+// The quotient variable should only be assigned once
+const quotient = 4.4 / 2.0; // Change this line
