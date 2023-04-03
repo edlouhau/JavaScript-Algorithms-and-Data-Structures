3723dc6240ba11dbcdb0a8d7c4e645242c27e9d8
Eduardo Louhau <louhaudev@gmail.com>
2023-04-03 18:04:45 -0400

    ex-22

---
+++ Basic/ex-22-compound-subtraction.js
@@ -0,0 +1,14 @@
+// a should equal 5.
+// b should equal -6.
+// c should equal 2.
+// You should use the -= operator for each variable.
+// You should not modify the code above the specified comment.
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
