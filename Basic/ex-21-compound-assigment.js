af72583b17a37e588bfbc45a226f8baea95e4874
Eduardo Louhau <louhaudev@gmail.com>
2023-04-03 17:58:45 -0400

    ex-21

---
+++ Basic/ex-21-compound-assigment.js
@@ -0,0 +1,14 @@
+// a should equal 15.
+// b should equal 26.
+// c should equal 19.
+// You should use the += operator for each variable.
+// You should not modify the code above the specified comment.
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
