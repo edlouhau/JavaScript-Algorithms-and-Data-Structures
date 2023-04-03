20c14a889a1db440e2cad6edc95bff0a8e169a7e
Eduardo Louhau <louhaudev@gmail.com>
2023-04-03 17:40:29 -0400

    ex-16

---
+++ Basic/ex-16-decrement-a-number.js
@@ -0,0 +1,9 @@
+// myVar should equal 10.
+// myVar = myVar - 1; should be changed.
+// You should not assign myVar with 10.
+// You should use the -- operator on myVar.
+// You should not change code above the specified comment.
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
