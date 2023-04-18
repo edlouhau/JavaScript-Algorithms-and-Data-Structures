7b5bc87c7f987db3b3442537f34a857acd5e43f9
Eduardo Louhau <louhaudev@gmail.com>
2023-04-17 21:22:50 -0400

    ex-24

---
+++ Basic/ex-24-augmented-division.js
@@ -0,0 +1,14 @@
+// a should equal 4.
+// b should equal 27.
+// c should equal 3.
+// You should use the /= operator for each variable.
+// You should not modify the code above the specified comment.
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
