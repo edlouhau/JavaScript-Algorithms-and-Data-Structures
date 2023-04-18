459a494b397603663183158b9ac412d0d855149f
Eduardo Louhau <louhaudev@gmail.com>
2023-04-17 23:03:43 -0400

    ex-27

---
+++ Basic/ex-27-escape-sequences.js
@@ -0,0 +1,9 @@
+// myStr should not contain any spaces
+// myStr should contain the strings FirstLine, SecondLine and ThirdLine (remember case sensitivity)
+// FirstLine should be followed by the newline character \n
+// myStr should contain a tab character \t which follows a newline character
+// SecondLine should be preceded by the backslash character \
+// There should be a newline character between SecondLine and ThirdLine
+// myStr should only contain characters shown in the instructions
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
