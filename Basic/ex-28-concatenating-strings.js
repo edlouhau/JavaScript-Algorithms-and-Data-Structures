af13ccbe46c1287514b90f29ff7c48df74826df7
Eduardo Louhau <louhaudev@gmail.com>
2023-04-17 23:10:45 -0400

    ex-28

---
+++ Basic/ex-28-concatenating-strings.js
@@ -0,0 +1,7 @@
+// myStr should have a single space character between the two strings.
+// myStr should have a value of the string This is the start. This is the end.
+// You should use the + operator to build myStr.
+// myStr should be created using the const keyword.
+// You should assign the result to the myStr variable.
+
+const myStr = "This is the start." + " This is the end."; // Change this line
